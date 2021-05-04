8cd4a8103f937fcc3d38aa43554c2ac9ba663270
ubuntu <ubuntu@localhost.localdomain>
2021-05-04 22:07:21 +0800

    Add build

--- scripts.sh
+++ scripts.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 # run: bundle exec jekyll serve -H 0.0.0.0
-bundle exec jekyll build
+
+cd $SCRIPT_DIR
+
+bundle exec jekyll build --watch
